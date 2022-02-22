761344457699f05113af82c215800d81f74ccacb
An7112 <thanhan7112@gmail.com>
2022-02-23 01:43:20 +0700

    HomeDatabase

--- flutter_database/lib/HomeDatabase.dart
+++ flutter_database/lib/HomeDatabase.dart
@@ -1,15 +1,81 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_database/DatabaseCustom.dart';
 
 class HomeDatabase extends StatefulWidget {
-  HomeDatabase({Key? key}) : super(key: key);
-
   @override
-  State<HomeDatabase> createState() => _HomeDatabaseState();
+  _HomeDatabaseState createState() => _HomeDatabaseState();
 }
 
 class _HomeDatabaseState extends State<HomeDatabase> {
+  final fb = FirebaseDatabase.instance;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final ref = fb.ref().child('users');
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigo[900],
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => Database(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Users',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: FirebaseAnimatedList(
+        query: ref,
+        shrinkWrap: true,
+        itemBuilder: (context, snapshot, animation, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  tileColor: Colors.indigo[100],
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.red[900],
+                    ),
+                    onPressed: () {
+                      ref.child(snapshot.key!).remove();
+                    },
+                  ),
+                  title: Text(
+                    snapshot.value.toString(),
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
