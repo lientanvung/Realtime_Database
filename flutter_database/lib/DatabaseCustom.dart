a64ddfae219e79332af863cc412962931b47075d
An7112 <thanhan7112@gmail.com>
2022-02-23 00:17:48 +0700

    custom database

---
+++ flutter_database/lib/DatabaseCustom.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Database extends StatefulWidget {
+  Database({Key? key}) : super(key: key);
+
+  @override
+  State<Database> createState() => _DatabaseState();
+}
+
+class _DatabaseState extends State<Database> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
